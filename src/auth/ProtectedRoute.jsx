79454028f3bc1c34d5a3d24b803713ef9a9180e7
jagdishlove <jagdish.funn@gmail.com>
2026-06-09 12:48:59 +0100

    updated loading

--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
+import LoadingBar from "../components/LoadingBar";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, loading } = useAuth();
 
   if (loading) {
-    return <div>Loading...</div>; // Or a spinner component
+    return <LoadingBar />; // Or a spinner component
   }
 
   if (!isAuthenticated) {
